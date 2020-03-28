19c2d52a21a1ecdc67938705230e7ace4be5bffc
vyshka97 <maks081197@yandex.ru>
2020-03-28 22:15:34 +0300

    New requirements, ingore folders

--- brain_morph.py
+++ brain_morph.py
@@ -1,3 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from app import flask_app
+from app.model import PatientCollection
+
+
+@flask_app.shell_context_processor
+def make_shell_context():
+    return {PatientCollection.__name__: PatientCollection}
